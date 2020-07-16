0c2574e8566c69817b53c4adb28a59f811eb5c77
Konstantin Suslov <suslov93@gmail.com>
2020-07-16 13:12:03 +0300

    Module04 homework
    Signed-off-by: Konstantin Suslov <suslov93@gmail.com>

--- Module04-AsyncPart2/AwaitableExercises.Core/BoolAwaiter.cs
+++ Module04-AsyncPart2/AwaitableExercises.Core/BoolAwaiter.cs
@@ -5,13 +5,24 @@ namespace AwaitableExercises.Core
 {
     public static class BoolExtensions
     {
+        public static BoolAwaiter GetAwaiter(this bool @bool) => new BoolAwaiter(@bool);
     }
 
     public class BoolAwaiter : INotifyCompletion
     {
+        private readonly bool _bool;
+
+        public BoolAwaiter(bool @bool)
+        {
+            _bool = @bool;
+        }
+
         public void OnCompleted(Action continuation)
         {
-            throw new NotImplementedException();
+            continuation();
         }
+
+        public bool GetResult() => _bool;
+        public bool IsCompleted => true;
     }
 }
