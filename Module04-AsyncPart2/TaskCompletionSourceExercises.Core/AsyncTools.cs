1bc15e570a0819ecfc7179766fb9388e5530e459
Konstantin Suslov <suslov93@gmail.com>
2020-07-26 15:58:35 +0300

    Module04 homework part2

--- Module04-AsyncPart2/TaskCompletionSourceExercises.Core/AsyncTools.cs
+++ Module04-AsyncPart2/TaskCompletionSourceExercises.Core/AsyncTools.cs
@@ -1,14 +1,39 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace TaskCompletionSourceExercises.Core
 {
     public class AsyncTools
     {
-        public static Task<string> RunProgramAsync(string path, string args = "")
+        public static Task<string> RunProgramAsync(string path, string args = null)
         {
-            return Task.FromResult(string.Empty);
+            var tcs = new TaskCompletionSource<string>(TaskCreationOptions.RunContinuationsAsynchronously);
+            
+            var process = new Process
+            {
+                EnableRaisingEvents = true,
+                StartInfo = new ProcessStartInfo(path, args)
+                {
+                    RedirectStandardOutput = true, 
+                    RedirectStandardError = true
+                }
+            };
+
+            process.Exited += (sender, eventArgs) =>
+            {
+                var senderProcess = sender as Process;
+                if (senderProcess?.ExitCode != 0)
+                    tcs.SetException(new Exception(senderProcess?.StandardError.ReadToEnd()));
+                else
+                    tcs.SetResult(senderProcess?.StandardOutput.ReadToEnd());
+
+                senderProcess?.Dispose();
+            };
+            process.Start();
+
+            return tcs.Task;
         }
     }
 }
